c81570b9ddc63b68bbc6f503fa354b8412ad2e5a
marinapeng <135253288+marinapeng@users.noreply.github.com>
2023-11-06 21:12:12 -0700

    Add files via upload

---
+++ Magpie2.java
@@ -0,0 +1,114 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ *             Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+    /**
+     * Get a default greeting     
+     * @return a greeting
+     */
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
+    
+    /**
+     * Gives a response to a user statement
+     * 
+     * @param statement
+     *            the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String response = "";
+        String num = (statement.trim());
+        if (num.length() < 1)
+        {
+            response = "Say something please";
+        }
+        else if (statement.indexOf("no") >= 0)
+        {
+            response = "Why so negative?";
+        }
+        else if (statement.indexOf("mother") >= 0
+                || statement.indexOf("father") >= 0
+                || statement.indexOf("sister") >= 0
+                || statement.indexOf("brother") >= 0)
+        {
+            response = "Tell me more about your family.";
+        }
+        else
+        {
+            response = getRandomResponse();
+        }
+         if (statement.indexOf("cat") != -1 || statement.indexOf("dog") != -1)
+        {
+            response = "Tell me more about your pets";
+        }
+        if (statement.indexOf("Mr.") !=-1 )
+        {
+            response = "He sounds like a good teacher";
+        }
+        else if (statement.indexOf("Mrs.") !=-1)
+        {
+            response = "She sounds like a good teacher";
+        }
+        if (statement.indexOf("brother") !=-1)
+        {
+            response = "How old is he?";
+        }
+        if (statement.indexOf("tree") !=-1)
+        {
+            response = "What species is it?";
+        }
+        return response;
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 6;
+        double r = Math.random();
+        int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+        String response = "";
+        
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        }
+        else if (whichResponse == 4)
+        {
+            response = "That sounds interesting.";
+        }
+        else if (whichResponse == 5)
+        {
+            response = "No";
+        }
+        return response;
+    }
+}
